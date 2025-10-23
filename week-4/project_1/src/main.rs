9ca7c33ce8df9762c960bb4af9e712c50e66ad43
Ini-db-19 <inioluwa.idowu@pau.edu.ng>
2025-10-23 17:48:06 +0100

    Updated my folder

---
+++ week-4/project_1/src/main.rs
@@ -0,0 +1,41 @@
+// Rust program to solve quadratic roots and determinants
+
+use std::io;
+
+fn main () { 
+
+let mut input1 = String::new();
+let mut input2 = String::new();
+let mut input3 = String::new();
+
+println!("Enter the value of a:");
+io::stdin().read_line(&mut input1).expect("Not a valid string");
+let a:f32 = input1.trim().parse().expect("Not valid decimal");
+
+println!("Enter the value of b:");
+io::stdin().read_line(&mut input2).expect("Not a valid string");
+let b:f32 = input2.trim().parse().expect("Please enter a decimal");
+
+println!("Enter the value of c:");
+io::stdin().read_line(&mut input3).expect("Not a valid string");
+let c:f32 = input3.trim().parse().expect("Not a valid decimal");
+
+//Computing the discriminant
+let discriminant = b.powf(2.0) - (4.0*a*c);
+println!("The discriminant is: {}", discriminant);
+
+if discriminant > 0.0 {
+    let root1 = (-b + discriminant.sqrt()) / (2.0 * a);
+    let root2 = (-b - discriminant.sqrt()) / (2.0 * a);
+    println!("Two distinct real roots: x1 = {}, x2 = {}", root1, root2);
+
+} else if discriminant == 0.0 {
+    let root = -b / (2.0 * a);
+    println!("One real root: x = {}", root); 
+
+} else {
+    println!("No real roots");
+}
+
+
+}
