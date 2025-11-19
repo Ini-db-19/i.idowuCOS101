692d3c9f33d9169e8c662d670e9420b0bd3983a4
Ini-db-19 <inioluwa.idowu@pau.edu.ng>
2025-11-19 17:00:58 +0100

    Updating a vector

---
+++ week-8/practice_5/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    // Create an empty vector "City"
+    let mut city : Vec<String> = Vec::new();
+
+    // Print City Vector
+    println!("The City vector has element {}",city.len());
+
+    //Push new elements into
+    let mut input1 = String::new();
+    println!("How many cities do you want to input?");
+    std::io::stdin().read_line(&mut input1).expect("Failed to read input");
+    let city_num:i32 = input1.trim().parse().expect("Invalid integer");
+
+    for count in 0..city_num {
+        let mut input2 = String::new();
+        println!("Enter City {}", count+1);
+        std::io::stdin().read_line(&mut input2).expect("Invalid integer");
+
+        let new_city:String = input2.trim().parse().expect("This is not a valid input");
+        city.push(new_city);
+    }
+    print!("Your preferred cities are:\n");
+
+    let mut count = 1;
+
+    //loop to iterate elements in vector
+    for i in city {
+        // iterating through i on the vector
+        println!("{} {}",count, i);
+        count+=1
+    }
+
+}
